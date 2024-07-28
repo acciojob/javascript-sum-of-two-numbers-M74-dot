518549bb62e5c3bcd8c8d00d0ee6352599b3b7cf
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 21:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+// let input1 = prompt("Enter the first number:");
+// let input2 = prompt("Enter the second number:");
 
 if(isNaN(input1) && isNaN(input2)){
 	alert("Invalid input. Please enter a valid number.");
 }else{
-	alert(Number(input1)+Number(input2));
+	let ans = Number(input1)+Number(input2)
+	alert(ans);
 }
 // Write your code here and print the output using alert function
