3338b416ca708a47a0fc927ad19f7a71b88a1024
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-29 19:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+// let input1 = prompt("Enter the first number:");
+// let input2 = prompt("Enter the second number:");
 
-if (input1 === null || input2 === null || input1.trim() === "" || input2.trim() === "" || isNaN(input1) || isNaN(input2)) {
+if (input1 === null || input2 === null || input1.trim() === "" || input2.trim() === "" || isNaN(parseFloat(input1)) || isNaN(parseFloat(input2))) {
     alert("Invalid input. Please enter a valid number.");
 } else {
     let num1 = parseFloat(input1);
